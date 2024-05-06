f76ddef744a562b9d5dce0b8f7a7ec3395347e92
D-pixel-crime <dpk4383@gmail.com>
2024-05-06 11:19:36 +0530

    minor changes

--- backend/dist/routes/devices.js
+++ backend/dist/routes/devices.js
@@ -7,45 +7,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import axios from "axios";
 import express from "express";
-import Devices from "../schema/devices.js";
+import parser from "ua-parser-js";
 const router = express.Router();
 router.get("/:userId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const userId = req.params.userId;
-    console.log(userId);
     const userAgent = req.headers["user-agent"];
-    try {
-        const deviceData = yield Devices.find({ user: userId });
-        if (!deviceData) {
-            res.status(404).json({ error: "No devices found for this user" });
-        }
-        const isAllReadyThere = deviceData.find((device) => device.fullInfo === userAgent);
-        if (isAllReadyThere) {
-            res.status(200).json({ allDevices: deviceData });
-        }
-        else {
-            const { data } = yield axios.post("https://api.apicagent.com", { ua: userAgent }, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            const thisDevice = yield Devices.create({
-                fullInfo: userAgent,
-                user: userId,
-                deviceType: data.device.type,
-                deviceBrand: data.device.brand,
-                deviceModel: data.device.model,
-                clientType: data.client.type,
-                browserName: data.client.name,
-                isLoggedIn: true,
-                lastLogin: new Date().toUTCString(),
-            });
-            res.status(200).json({ allDevices: [...deviceData, thisDevice] });
-        }
-    }
-    catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+    console.log(parser(userAgent));
+    res.status(200).send("OK");
 }));
 export { router };
--- backend/routes/devices.ts
+++ backend/routes/devices.ts
@@ -2,55 +2,16 @@ import axios from "axios";
 import express from "express";
 import Devices from "../schema/devices.js";
 import { log } from "console";
+import parser from "ua-parser-js";
 
 const router = express.Router();
 
 router.get("/:userId", async (req, res) => {
   const userId = req.params.userId;
-  console.log(userId);
-
   const userAgent = req.headers["user-agent"];
 
-  try {
-    const deviceData = await Devices.find({ user: userId });
-
-    if (!deviceData) {
-      res.status(404).json({ error: "No devices found for this user" });
-    }
-
-    const isAllReadyThere = deviceData.find(
-      (device) => device.fullInfo === userAgent
-    );
-
-    if (isAllReadyThere) {
-      res.status(200).json({ allDevices: deviceData });
-    } else {
-      const { data } = await axios.post(
-        "https://api.apicagent.com",
-        { ua: userAgent },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const thisDevice = await Devices.create({
-        fullInfo: userAgent,
-        user: userId,
-        deviceType: data.device.type,
-        deviceBrand: data.device.brand,
-        deviceModel: data.device.model,
-        clientType: data.client.type,
-        browserName: data.client.name,
-        isLoggedIn: true,
-        lastLogin: new Date().toUTCString(),
-      });
-
-      res.status(200).json({ allDevices: [...deviceData, thisDevice] });
-    }
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
+  console.log(parser(userAgent));
+  res.status(200).send("OK");
 });
 
 export { router };
